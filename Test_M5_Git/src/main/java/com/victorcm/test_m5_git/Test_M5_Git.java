d712f303e5f79ec427d10ebd1d644c1cd9b8cf0d
Víctor <Vctor@DESKTOP-UL9H8LU>
2023-12-04 17:32:34 +0100

    Added Java project

---
+++ Test_M5_Git/src/main/java/com/victorcm/test_m5_git/Test_M5_Git.java
@@ -0,0 +1,11 @@
+package com.victorcm.test_m5_git;
+
+/**
+ * @author Víctor
+ */
+public class Test_M5_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
